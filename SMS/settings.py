aa064585890133de9667f43c7c7db564413cc736
Rustam Karimov <rustemk1981@gmail.com>
2021-01-03 09:20:23 +0200

    Custom User model has been implemented in a new 'accounts' app

--- SMS/settings.py
+++ SMS/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL = 'accounts.User'
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
